1e71441ab0de710a249faee59f78404b1e52ec90
vlad05x <138985102+vlad05x@users.noreply.github.com>
2023-10-01 21:24:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let display = document.querySelector(".display");
+let buttons = Array.from(document.querySelectorAll(".button"));
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "AC":
+        display.innerText = "0";
+        break;
+      case "=":
+        display.innerText = eval(display.innerText);
+        break;
+      case "%":
+        let passedText = display.innerText + "/100";
+        display.innerText = eval(passedText);
+        break;
+      case "+/-":
+        display.innerText = "-";
+        break;
+      default:
+        if (display.innerText == "0" && e.target.innerText !== ".") {
+          display.innerText = e.target.innerText;
+        } else {
+          display.innerText += e.target.innerText;
+        }
+    }
+  });
+});

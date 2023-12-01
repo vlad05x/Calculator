bcd08ffb6bc400de659dde254d934eb46aa5276c
vlad05x <organizm777alf@gmail.com>
2023-12-01 18:13:07 +0200

    Fix bugs and add buttin " C-clear"

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let display = document.querySelector(".display");
 let buttons = Array.from(document.querySelectorAll(".button"));
-
+let operatorClicked = false;
 buttons.map((button) => {
   button.addEventListener("click", (e) => {
     switch (e.target.innerText) {
@@ -14,14 +14,23 @@ buttons.map((button) => {
         let passedText = display.innerText + "/100";
         display.innerText = eval(passedText);
         break;
-      case "+/-":
-        display.innerText = "-";
+      case "C":
+        let currentNumber = display.innerText;
+        display.innerText = currentNumber.slice(0, -1);
+        if (display.innerText === "") {
+          display.innerText = "0";
+        }
         break;
       default:
-        if (display.innerText == "0" && e.target.innerText !== ".") {
-          display.innerText = e.target.innerText;
+        if (e.target.innerText.match(/[+\-*/]/) && operatorClicked) {
+          display.innerText = "Error";
         } else {
-          display.innerText += e.target.innerText;
+          if (display.innerText == "0" && e.target.innerText !== ".") {
+            display.innerText = e.target.innerText;
+          } else {
+            display.innerText += e.target.innerText;
+          }
+          operatorClicked = e.target.innerText.match(/[+\-*/]/) ? true : false; // Устанавливаем флаг, если нажат оператор
         }
     }
   });
